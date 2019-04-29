38012f887fe5a2f1df4c21769f3a4f0e14048112
Sedat <sedatsayin_fb@hotmail.com>
2019-04-29 12:21:44 +0300

    first
    responsive image slider

---
+++ Assets/myScripts/custom.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    //slider
+    var slider_items = $('.slider-item ul.slider-item-container > li');
+    slider_items.filter(':not(:first)').hide();
+    slider_items.filter(':first').addClass('active');
+});
+
+function changeSlider(to) {
+    $(".sayfaSlider").fadeOut(500).removeClass('active');
+    $("#" + to).fadeIn(500).addClass('active');
+}
+
+function changeSliderButton(to) {
+    $(".clickSlider").removeClass('active');
+    $("#" + to).addClass('active');
+}
